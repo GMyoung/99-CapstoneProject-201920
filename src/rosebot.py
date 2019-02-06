814ea94b6933ce159eb9b0f68b395f5a4f1c3070
GMyoung <yangy9@rose-hulman.edu>
2019-02-06 09:32:48 -0500

    fix bug of firs lines of code in rosebot and sensor

--- src/rosebot.py
+++ src/rosebot.py
@@ -30,9 +30,9 @@ import math
 class RoseBot(object):
     def __init__(self):
         # Use these instance variables
-        self.arm_and_claw = ArmAndClaw()
         self.sensor_system = SensorSystem()
         self.drive_system = DriveSystem(self.sensor_system)
+        self.arm_and_claw = ArmAndClaw(self.sensor_system.touch_sensor)
 
 
 ###############################################################################
@@ -200,16 +200,16 @@ class ArmAndClaw(object):
         """ Raises the Arm until its touch sensor is pressed. """
 
     def calibrate_arm(self):
-        self. motor.turn_on(100)
-        while True:
-            if self.touch_sensor.is_pressed():
-                self.motor.turn_off()
-                break
-        self. motor.turn_on(-100)
-        while True:
-            if self.motor.():
-                self.motor.turn_off()
-                break
+        # self. motor.turn_on(100)
+        # while True:
+        #     if self.touch_sensor.is_pressed():
+        #         self.motor.turn_off()
+        #         break
+        # self. motor.turn_on(-100)
+        # while True:
+        #     if self.motor.():
+        #         self.motor.turn_off()
+        #         break
 
 
 
@@ -244,15 +244,14 @@ class SensorSystem(object):
     the Button objects that form part of the BeaconSystem and DisplaySystem.
     Use this object to get   ** any **   sensor reading.
     """
-    def __init__(self, touch_sensor, color_sensor, ir_proximity_sensor,
-                 ir_beacon_sensor, camera, beacon_system, display_system):
-        self.touch_sensor = touch_sensor
-        self.color_sensor = color_sensor
-        self.ir_proximity_sensor = ir_proximity_sensor
-        self.ir_beacon_sensor = ir_beacon_sensor
-        self.camera = camera
-        self.beacon_system = beacon_system
-        self.display_system = display_system
+    def __init__(self):
+        self.touch_sensor = TouchSensor(1)
+        # self.color_sensor = color_sensor
+        # self.ir_proximity_sensor = ir_proximity_sensor
+        # self.ir_beacon_sensor = ir_beacon_sensor
+        # self.camera = camera
+        # self.beacon_system = beacon_system
+        # self.display_system = display_system
 
 
 ###############################################################################
